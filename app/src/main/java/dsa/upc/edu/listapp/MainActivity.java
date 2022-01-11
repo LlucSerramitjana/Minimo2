9f1413e8fbb2b2c90f0f79c155c6da8ae629f359
LlucSerramitjana <lluc.serramitjana@estudiantat.upc.edu>
2022-01-11 09:22:00 +0100

    Prova commit

--- app/src/main/java/dsa/upc/edu/listapp/MainActivity.java
+++ app/src/main/java/dsa/upc/edu/listapp/MainActivity.java
@@ -114,7 +114,7 @@ public class MainActivity extends AppCompatActivity {
                 .load(user.getAvatar_url())
                 .into(avatar);
     }
-    //prova
+    //prova2
     private void doApiCall(final SwipeRefreshLayout mySwipeRefreshLayout, String username) {
         Call<List<Follower>> call = gitHub.followers(username);
         call.enqueue(new Callback<List<Follower>>() {
